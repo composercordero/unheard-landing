a56f68722b7e245066e4d487c6932caa48481a89
composercordero <composercordero@gmail.com>
2023-12-06 00:24:17 -0600

    laying out MVP sketch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,39 @@
 
 const Hero = () => {
     return(<>
-    
+        <div>
+            <h1>un/heard</h1>
+            <ul>
+                <li>Home</li>
+                <li>Our Mission</li>
+                <li>Media</li>
+                <li>Get in touch</li>
+                <li>Artists</li>
+            </ul>
+
+            <p>Elevating LGBTQ+ Voices Together</p>
+            <p>Video</p>
+        </div>
+
+        <div>
+            <h1>Why do we sing?</h1>
+            <p>We gather people through choral music to elevate the voices of the LGBTQ+ community. If you are wondering how we do this, read more right here.</p>
+        </div>
+
+        <div>
+            <p>video card</p>
+            <p>read more about this piece and the composer</p>
+        </div>
+
+        <div>
+            <h1>Let's get in touch</h1>
+            <form></form>
+        </div>
+
+        <footer>
+            
+        </footer>
+
     </>)
 };
 
